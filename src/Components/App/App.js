11d2353dc8589db5adac63801f92e1f3392abea5
katiescruggs <katie.e.scruggs@gmail.com>
2018-09-28 18:51:30 -0600

    Move boilerplate App into its own directory and make it display certain number of boxes

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Box from '../Box/Box';
+import Row from '../Row/Row';
+import './App.css';
+
+const App = () => {
+  const rows = [];
+  const numberOfRows = 25;
+
+  for (let i = 0; i < numberOfRows; i++) {
+    rows.push(<Box />);
+  }
+
+  return (
+    <div className="App">
+      {rows}
+    </div>
+  );
+}
+
+
+export default App;
